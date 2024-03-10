067a6405144263543ede41a2d5c35504cc2b1255
ajhmdni <ajhmdni02@gmail.com>
2024-03-10 07:22:01 +0700

    (update): attribute model changes

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -128,15 +128,63 @@ class UserController extends Controller
          | ]); 
          */
 
-        $user = UserModel::firstOrNew([
-            'username' => 'manager33',
-            'nama' => 'Manager TIga Tiga Tiga',
-            'password' => Hash::make('12345'),
-            'level_id' => 2
-        ]);
-        $user->save();
+        /*
+         | RETRIEVE AND CREATING MODEL, SAVE TO STORE DATA
+         |
+         | $user = UserModel::firstOrNew([
+         |    'username' => 'manager33',
+         |    'nama' => 'Manager TIga Tiga Tiga',
+         |    'password' => Hash::make('12345'),
+         |    'level_id' => 2
+         | ]);
+         | $user->save();
+         */
+
+        /*
+         | ATTRIBUTE CHANGES [CLEAN OR DIRTY]
+         |
+         | $user = UserModel::create([
+         |   'username' => 'manager44',
+         |   'nama' => 'Manager44',
+         |   'password' => Hash::make('12345'),
+         |   'level_id' => 2,
+         | ]);
+         |
+         | $user->username = 'manager45';
+         | 
+         | $user->isDirty(); // true
+         | $user->isDirty('username'); // true
+         | $user->isDirty('nama'); // false
+         | $user->isDirty(['nama', 'username']); // true
         
-        return view('user', ['data' => $user]);
+         | $user->isClean(); // false
+         | $user->isClean('username'); // false
+         | $user->isClean('nama'); // true
+         | $user->isClean(['nama', 'username']); // false
+
+         | $user->save();
+
+         | $user->isDirty();
+         | $user->isClean();
+         | dd($user->isClean()); 
+         */
+
+        $user = UserModel::create([
+            'username' => 'manager11',
+            'nama' => 'Manager11',
+            'password' => Hash::make('12345'),
+            'level_id' => 2,
+        ]);
+
+        $user->username = 'manager12';
+
+        $user->save();
+
+        $user->wasChanged();
+        $user->wasChanged('username');
+        $user->wasChanged(['username', 'level_id']);
+        $user->wasChanged('nama');
+        dd($user->wasChanged(['nama', 'username']));
     }
 }
 
