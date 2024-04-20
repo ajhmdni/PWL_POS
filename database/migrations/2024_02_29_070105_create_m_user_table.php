ef56f8e67f65dd01f08c4bf6fec7895fd108720e
ajhmdni <ajhmdni02@gmail.com>
2024-04-20 21:00:10 +0700

    (update): update dependencies

--- database/migrations/2024_02_29_070105_create_m_user_table.php
+++ database/migrations/2024_02_29_070105_create_m_user_table.php
@@ -19,7 +19,8 @@ return new class extends Migration
             $table->string('password');
             $table->timestamps();
 
-            $table->foreign('level_id')->references('level_id')->on('m_level');
+            $table->foreign('level_id')->references('level_id')->on('m_level')
+                ->cascadeOnDelete()->cascadeOnUpdate();
         });
     }
 
