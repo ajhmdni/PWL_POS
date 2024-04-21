f4d007ed9f9f9bcc0a6c670a1dba943e45a14834
ajhmdni <ajhmdni02@gmail.com>
2024-04-21 09:37:17 +0700

    feat(authentication & middleware): implementing authentication and middleware

--- app/Models/UserModel.php
+++ app/Models/UserModel.php
@@ -3,10 +3,10 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Model;
+use Illuminate\Foundation\Auth\User as Authenticable;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
-class UserModel extends Model
+class UserModel extends Authenticable
 {
     use HasFactory;
 
