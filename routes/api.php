fe1eb680fe804855af9af57f844c5a3c52c005b8
ajhmdni <ajhmdni02@gmail.com>
2024-04-30 23:17:36 +0700

    feat(auth login): Add Login Function for Authentication Method

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Api\LoginController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\RegisterController;
@@ -15,13 +16,12 @@ use App\Http\Controllers\Api\RegisterController;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
 
 Route::post('/register', RegisterController::class)->name('register');
-Route::get('/', function () {
-    return response()->json([
-        'hello' => 'world'
-    ]);
+Route::post('/login', LoginController::class)->name('login');
+Route::middleware('auth:api')->get('/user', function (Request $request) {
+    return $request->user();
 });
